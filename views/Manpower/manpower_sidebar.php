0838bfa3692ae9fadd5b147aea8d774f104766d7
Madhawa Dissanayake <73326224+MadhawaDissanayake@users.noreply.github.com>
2021-10-16 19:35:17 +0530

    Update manpower_sidebar.php

--- views/Manpower/manpower_sidebar.php
+++ views/Manpower/manpower_sidebar.php
@@ -1,10 +1,12 @@
 <?php ?>
 <div class="sidebar">
     <ul>
-        <li class="top"><a href="<?php echo fullURLfront; ?>/Manpower/manpower_dashboard"><i class="fa fa-search"></i> Search</a></li>
+        <li class="top"><a href="<?php echo fullURLfront; ?>/Manpower/manpower_dashboard"><i class="fa fa-dash"></i> Dashboard</a></li>
+        <li><a href="<?php echo fullURLfront; ?>/Manpower/manpower_search"><i class="fa fa-search"></i> Search</a></li>
         <li><a href="<?php echo fullURLfront; ?>/Manpower/manpower_profile"><i class="fa fa-user"></i> Profile</a></li>
-        <li><a href="<?php echo fullURLfront; ?>/Employee/employee_booking"><i class="fa fa-calendar"></i> Worker</a></li>
-        <li><a href="<?php echo fullURLfront; ?>/Employee/employee_booking"><i class="fa fa-calendar"></i> Booking</a></li>
+        <li><a href="<?php echo fullURLfront; ?>/Employee/employee_booking"><i class="fa fa-worker"></i> Worker</a></li>
+        <li><a href="<?php echo fullURLfront; ?>/Employee/employee_booking"><i class="fa fa-calendar"></i> Calendar</a></li>
+        <li><a href="<?php echo fullURLfront; ?>/Employee/employee_booking"><i class="fa fa-booking"></i> Booking</a></li>
         <li><a href="<?php echo fullURLfront; ?>/Employee/employee_complaint"><i class="fa fa-frown-o"></i> Complaints</a></li>
         <li><a href="<?php echo fullURLfront; ?>/Employee/employee_history"><i class="fa fa-history"></i> History</a></li>
         <li><a href="<?php echo fullURLfront; ?>/Employee/employee_help"><i class="fa fa-info-circle"></i> Help</a></li>
