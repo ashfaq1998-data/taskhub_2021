603180b2d70dca6e9c6e0bce4f35d3df61e98c2f
Thisara Dilshan Hewage <thisarad582@gmail.com>
2021-12-17 08:29:02 +0530

    taskhub new mp files 12/17

---
+++ views/Manpower/manpower_sidebar.php
@@ -0,0 +1,12 @@
+<?php ?>
+<div class="sidebar">
+    <ul>
+        <li class="top"><a href="#"><i class="fa fa-search"></i> Search</a></li>
+        <li><a href="<?php echo fullURLfront; ?>/Manpower/manpower_profile"><i class="fa fa-user"></i> Profile</a></li>
+        <li><a href="<?php echo fullURLfront; ?>/Manpower/manpower_booking"><i class="fa fa-calendar"></i> Booking</a></li>
+        <li><a href="<?php echo fullURLfront; ?>/Manpower/manpower_complaint"><i class="fa fa-frown-o"></i> Complaints</a></li>
+        <li><a href="#"><i class="fa fa-history"></i> History</a></li>
+        <li><a href="<?php echo fullURLfront; ?>/Manpower/manpower_help"><i class="fa fa-info-circle"></i> Help</a></li>
+        <li><a href="#"><i class="fa fa-audio-description"></i> View Advertsiment</a></li>
+    </ul>
+</div>
