d82a8eaac1a3c94742e2fca24ffddfad4d42c05a
udesh98 <um4me.98@gmail.com>
2022-01-24 22:21:23 +0530

    started pagination in search function

--- views/Customer/customer_servicelist.php
+++ views/Customer/customer_servicelist.php
@@ -4,6 +4,19 @@
     $details = $data['customer_search'];
     $arrLength = count($details);
     $err = $details['error'];
+
+    $num_per_page = 02;
+    $num_rows = $details['num_rows'];
+    $total_pages = ceil($num_rows/$num_per_page);
+
+    if(isset($_GET["page"])){
+        $page = $_GET["page"];
+        $i = $page*$num_per_page - $num_per_page;
+    }
+    else{
+        $page = 1;
+        $i = $page*$num_per_page - $num_per_page;
+    }
 ?>
 
 <!DOCTYPE html>
